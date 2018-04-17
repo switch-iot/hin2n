05e1b9e7b074b56f8bf2b0a468dadba1399170f5
switch_st <switch.st@gmail.com>
2018-04-18 00:28:58 +0800

    commit link.bat script for windows `JUNCTION` folder links

---
+++ link.bat
@@ -0,0 +1,19 @@
+@echo off
+
+call:chklink n2n_v1 bundles\n2n_meyerd\n2n_v1
+call:chklink n2n_v2 bundles\n2n_ntop
+call:chklink n2n_v2s bundles\n2n_meyerd\n2n_v2
+call:chklink uip bundles\uip\uip
+pushd An2n\app\src\main\cpp
+call:chklink n2n_v2s ..\..\..\..\..\n2n_v2s
+call:chklink uip ..\..\..\..\..\uip
+popd
+goto:eof
+
+:chklink
+if exist %~1 (
+	pushd %~1 2>nul && popd || (del /q %~1 & mklink /j %~1 %~2)
+) else (
+	mklink /j %~1 %~2
+)
+goto:eof
