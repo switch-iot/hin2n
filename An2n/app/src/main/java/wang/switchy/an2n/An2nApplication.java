16aedbca5678f45cc28c8b2121f99fefbfd15206
zhangbz <zhangbz.happyhacking@gmail.com>
2018-04-20 23:43:30 +0800

    modify gradle for build package

--- An2n/app/src/main/java/wang/switchy/an2n/An2nApplication.java
+++ An2n/app/src/main/java/wang/switchy/an2n/An2nApplication.java
@@ -20,7 +20,9 @@ public class An2nApplication extends Application {
 
         AppContext = this;
 
-        UMConfigure.init(this, "5ad8aba3a40fa373830002f5", "Umeng", UMConfigure.DEVICE_TYPE_PHONE, null);
+//        UMConfigure.init(this, "5ad8aba3a40fa373830002f5", "Umeng", UMConfigure.DEVICE_TYPE_PHONE, null);
+
+        UMConfigure.init(this, UMConfigure.DEVICE_TYPE_PHONE, null);
         MobclickAgent.setScenarioType(this, MobclickAgent.EScenarioType.E_UM_NORMAL);
 //        MobclickAgent.setSecret(this, );
     }
